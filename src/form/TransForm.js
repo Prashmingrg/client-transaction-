09049d4df45eb260f0661e5d71a6a4631aa4c238
Prashmin Gurung <prashmingrg@gmail.com>
2022-12-06 09:26:00 +1100

    UI design ready

--- src/form/TransForm.js
+++ src/form/TransForm.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
-
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+const intialState = {
+  type: "",
+  name: "",
+  Amount: "",
+};
+
 export const TransForm = () => {
-  const [trans, setTrans] = useState({});
+  const [trans, setTrans] = useState(intialState);
 
   const handleOnChange = (e) => {
     const { name, value } = e.target;
@@ -14,49 +19,62 @@ export const TransForm = () => {
       ...trans,
       [name]: value,
     });
-    console.log(value);
   };
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
     console.log(trans);
+
+    setTrans(intialState);
   };
 
   return (
     <Form onSubmit={handleOnSubmit}>
       <Row className="g-4">
-        <Col md>
-          <Form.Select>
+        <Col md="2">
+          <Form.Select
+            defaultValue="Choose..."
+            name="type"
+            required
+            onChange={handleOnChange}
+            value={trans.type}
+          >
+            <option>choose...</option>
             <option>Income</option>
-            <option>Travel</option>
+
             <option>Expenses</option>
-            <option>Food</option>
           </Form.Select>
         </Col>
-        <Col md>
+        <Col md="5">
           <Form.Group>
             <Form.Control
               placeholder="Enter your transaction"
               onChange={handleOnChange}
               name="name"
+              required
+              value={trans.name}
             />
           </Form.Group>
         </Col>
-        <Col md>
+        <Col md="2">
           <Form.Group>
             <Form.Control
-              placeholder="Enter your Amount"
+              placeholder="Amount"
               onChange={handleOnChange}
-              name="amount"
+              name="Amount"
+              required
+              value={trans.Amount}
             />
           </Form.Group>
         </Col>
-        <Col md>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>{" "}
+        <Col md="1">
+          <div className="d-grid">
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </div>
         </Col>
-      </Row>{" "}
+      </Row>
     </Form>
   );
 };
